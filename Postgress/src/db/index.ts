3f72ab46e7494dcc6853f027a0cf4f774976665d
Dev <devvarshney11@gmail.com>
2024-07-03 14:34:34 +0530

    Hidding the url of the Postgress database

--- Postgress/src/db/index.ts
+++ Postgress/src/db/index.ts
@@ -1,9 +1,7 @@
 import { Client } from "pg";
 
 export async function getClient() {
-  const client = new Client(
-    "postgresql://Test_owner:0qwCbmSv1lxT@ep-lively-moon-a5usd6x8.us-east-2.aws.neon.tech/Test?sslmode=require"
-  );
+  const client = new Client("DATABASE_URL");
   await client.connect();
   return client;
 }
